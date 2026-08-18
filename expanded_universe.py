086a0c4af157696862997a40c7021765192e5fd7
Taxi77777 <ouicab77@gmail.com>
2026-08-18 19:50:38 +0200

    feat: expand Daily + 1H investable universe

---
+++ expanded_universe.py
@@ -0,0 +1,7 @@
+"""Expanded investable universe for Daily + 1H long-term scanner."""
+from __future__ import annotations
+
+EXPANDED_SYMBOLS = [
+    "AAPL","MSFT","NVDA","AMZN","META","GOOGL","GOOG","AVGO","TSLA","BRK-B",
+    "LLY","JPM","V","ORCL","MA","NFLX","WMT","COST","JNJ","HD","PG","ABBV","BAC","CRM","CVX","KO","CSCO","AMD","PM","GE","IBM","UNH","LIN","ABT","MCD","INTU","NOW","TXN","QCOM","CAT","AMGN","ACN","PEP","GS","ISRG","RTX","TMO","PLD","MRK","ADBE","BKNG","SPGI","AXP","BLK","PFE","SYK","LOW","TJX","DE","NEE","COP","XOM","SLB","EOG","OXY","MPC","VLO","PSX","CVS","GILD","REGN","VRTX","ELV","CI","BSX","MDT","ZTS","LMT","NOC","GD","BA","HWM","ETN","EMR","PH","UPS","UNP","HON","MMM","DELL","MU","MRVL","SMCI","ARM","TSM","ASML","ADI","LRCX","KLAC","AMAT","PANW","CRWD","FTNT","NET","DDOG","SNOW","PLTR","SHOP","UBER","ABNB","BKNG","DIS","CMCSA","T","VZ","TMUS","TGT","NKE","SBUX","MELI","PDD","NVO","NVS","SAP","AZN","SHEL","BABA","BIDU","SONY","SPOT","COIN","HOOD","PYPL","XYZ","SOFI","MSTR","RIOT","MARA","RIVN","F","GM","NIO","XPEV"
+]
